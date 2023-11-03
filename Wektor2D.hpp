adfeaabe022167596dca931d173c143c03075605
piterx001 <149515937+piterx001@users.noreply.github.com>
2023-11-03 13:56:59 +0100

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,59 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    
+    public:
+
+    int populacja(){return num_wek;};
+
+    static Wektor2D kart(double x, double y)
+    {
+        return Wektor2D(x, y);
+    }
+   
+    static Wektor2D bieg(double r, double alfa)
+    {   
+        int x, y;
+        x = r*cos(alfa);
+        y = r*sin(alfa);
+        return Wektor2D(x, y);
+    } 
+
+    void norm()
+    {
+        double norma;
+        norma = sqrt(x*x+y*y);
+        std::cout <<"Norma wynosi "<<norma<<std::endl;
+    }
+    void print()
+    {
+        std::cout<<"Wspołrzędne wektora to x = "<<x<<"; y = "<<y<<std::endl;
+    } 
+    
+    ~Wektor2D()
+    {
+        //--num_wek;
+        std::cout<<"Usunięto wektor o x ="<<x<<"; y = "<<y<<std::endl;
+    }
+    Informer cd;
+
+  
+    void SetY(int wspy){y = wspy;};
+    void SetX(int wspx){x = wspx;};
+    int GetX(){return x;};
+    int GetY(){return y;};
+
+    private:
+    int x;
+    int y;
+    static int num_wek;
+
+      Wektor2D(int wspx, int wspy)
+    {
+    ++num_wek;
+    SetX(wspx);
+    SetY(wspy);
+    std::cout <<"Utworzono wektor o x ="<<x<<"; y = "<<y<<std::endl;
+    }
+    
+  
 };
